64fef7eec10053c572f870a695df74d658c54ada
DanielToft92 <toftgaming92@gmail.com>
2024-12-02 15:41:07 +0100

    New Update

--- index.php
+++ index.php
@@ -13,29 +13,37 @@ require "settings/init.php";
 	<meta name="copyright" content="Information om copyright">
 	
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-image: url('billeder/navbaggrund5.png'); background-size: cover; background-position: center;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Navbar</a>
+        <a class="navbar-brand" href="index.php" style="color: #ffffff">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav mx-auto">
-                <a class="nav-link" href="#">Om os</a>
-                <a class="nav-link" href="#">Kalender</a>
-                <a class="nav-link" href="opslagstavle.php">Opslagstavle</a>
+                <a class="nav-link" href="#" style="color: #ffffff">Om os</a>
+                <a class="nav-link" href="#" style="color: #ffffff">Kalender</a>
+                <a class="nav-link" href="opslagstavle.php" style="color: #ffffff">Opslagstavle</a>
             </div>
         </div>
     </div>
 </nav>
 
 
+<div class="banner text-center text-white py-5" style="background-image:
+url('billeder/træplanker.png'); background-size: cover; background-position: center; height: 300px;">
+    <h1>Velkommen til vores hjemmeside</h1>
+    <p>En grøn og bæredygtig løsning!</p>
+</div>
 
 
 

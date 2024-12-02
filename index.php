f4a270ca7e18cbd83b229df1ee68bd689fe8846d
DanielToft92 <toftgaming92@gmail.com>
2024-12-02 07:39:02 +0100

    New Update

--- index.php
+++ index.php
@@ -22,19 +22,20 @@ require "settings/init.php";
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link" href="#">Om os</a>
+                <a class="nav-link" href="#">Kalender</a>
                 <a class="nav-link" href="opslagstavle.php">Opslagstavle</a>
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
             </div>
         </div>
     </div>
 </nav>
+
 
 
 

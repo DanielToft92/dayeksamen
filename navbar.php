b8b28b571e5299bda2e75343885d1af38dc0b8e2
DanielToft92 <toftgaming92@gmail.com>
2024-12-09 20:07:45 +0100

    New Update

---
+++ navbar.php
@@ -0,0 +1,19 @@
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-image: url('billeder/footerbaggrund2.png'); background-size: cover; background-position: center;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"">Re-Trade</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav mx-auto">
+                <a class="nav-link" href="omos.php">Om os</a>
+                <a class="nav-link" href="kalender.php">Kalender</a>
+                <a class="nav-link" href="opslagstavle.php">Opslagstavle</a>
+            </div>
+        </div>
+    </div>
+</nav>
--- opslagstavle.php
+++ opslagstavle.php
@@ -19,23 +19,7 @@ require "settings/init.php";
 
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-image: url('billeder/footerbaggrund2.png'); background-size: cover; background-position: center;">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav mx-auto">
-                <a class="nav-link" href="omos.php">Om os</a>
-                <a class="nav-link" href="kalender.php">Kalender</a>
-                <a class="nav-link" href="opslagstavle.php">Opslagstavle</a>
-            </div>
-        </div>
-    </div>
-</nav>
+<?php include("navbar.php") ?>
 
 
 <div id="board">
@@ -43,39 +27,7 @@ require "settings/init.php";
 </div>
 
 
-<footer class="footer mt-auto py-3"
-        style="background-image: url('billeder/footerbaggrund2.png'); background-size: cover; background-position: center; color: #000000;">
-    <div class="container text-center">
-        <div class="row">
-            <div class="col-md-4 mb-3">
-                <h5 style="color: inherit;">Kontakt Os</h5>
-                <p style="color: inherit;">Email: re-trade@zealand.dk</p>
-                <p style="color: inherit;">Telefon: +45 12345678</p>
-                <p style="color: inherit;">Adresse: Søndre Badevej 1, 4600 Køge</p>
-            </div>
-            <div class="col-md-4 mb-3">
-                <h5 style="color: inherit;">Følg Os</h5>
-                <a href="#" style="color: inherit; text-decoration: none; margin: 0 10px;">
-                    <i class="fab fa-facebook"></i> Facebook
-                </a>
-                <a href="#" style="color: inherit; text-decoration: none; margin: 0 10px;">
-                    <i class="fab fa-instagram"></i> Instagram
-                </a>
-                <a href="#" style="color: inherit; text-decoration: none; margin: 0 10px;">
-                    <i class="fab fa-tik-tok"></i> Tik-Tok
-                </a>
-                <a href="#" style="color: inherit; text-decoration: none; margin: 0 10px;">
-                    <i class="fab fa-youtube"></i> Youtube
-                </a>
-            </div>
-            <div class="col-md-4 mb-3">
-                <h5 style="color: inherit;">Om Projektet</h5>
-                <p style="color: inherit;">Re-Trade er din platform til bæredygtig byttehandel.</p>
-                <p style="color: inherit;">"Byt dig til en bæredygtig fremtid"</p>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php include("footer.php") ?>
 
 
 
